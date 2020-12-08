a6daa12b7fa5ade125f4f7d0306cdba09ea23bdb
Clemence Ververs <c.ververs@gmail.com>
2020-12-08 21:48:51 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # project
+
+Setup HTML 1. create element for location 2. create element for wind
+
+// Identify API on the weather
+// Select the wind object
+// Display the wind object
+
+// Identify API for the map
--- app.js
+++ app.js
@@ -1,1 +1,55 @@
 console.log("working");
+let lat = null;
+let long = null;
+
+const getLocation = new Promise((res) => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log(position);
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+      console.log(position.coords);
+      const url = `http://api.weatherunlocked.com/api/current/${lat},${long}?app_id=f7ace40d&app_key=535832c168314b149a87d7d93369a990`;
+      getTemp(showTemp, url);
+    });
+  } else {
+    window.alert("Could not get location");
+  }
+});
+
+function getTemp(callback, url) {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((tempObject) => {
+      callback(tempObject);
+      console.log(tempObject);
+    });
+}
+
+function showTemp(tempObject) {
+  console.log(tempObject.temp_c);
+  const tempInsert = document.querySelector(".temperature-degree");
+  tempInsert.innerHTML = tempObject.temp_c;
+  const tempDescription = document.querySelector(".temperature-description");
+  tempDescription.innerHTML = tempObject.wx_desc;
+  const locationDescription = document.querySelector(".location-wind");
+  tempDescription.innerHTML = tempObject.wx_desc;
+  const windDescription = document.querySelector(".wind-direction");
+  windDescription.innerHTML = tempObject.winddir_compass;
+  const windSpeed = document.querySelector(".wind-speed");
+  windSpeed.innerHTML = tempObject.windspd_kts;
+  const icon = document.querySelector(".icon");
+  console.log(tempObject.wx_icon);
+  icon.src = `set/${tempObject.wx_icon}`;
+}
+
+let map;
+function createMap(position) {
+  const { latitude, longitude } = position.coords;
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: { lat: latitude, lng: longitude },
+  });
+}
